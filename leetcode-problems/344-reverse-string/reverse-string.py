c6cef74bb1dce2599a62b6fd09eaef6ecf9e5ec5
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-05-28 12:46:47 +0530

    Time: 173 ms (34.39%) | Memory: 20.8 MB (71.57%) - LeetSync

--- leetcode-problems/344-reverse-string/reverse-string.py
+++ leetcode-problems/344-reverse-string/reverse-string.py
@@ -3,12 +3,28 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        # Cheat sols 
+        s.reverse()
+        # s[:] = s[::-1] #and not s=s[::-1] modification should be inplace
+
+
+        # # sol 1
         # for i in range(len(s)//2):
         #     temp = s[i]
         #     s[i] = s[(len(s)-1)-i]
         #     s[(len(s)-1)-i] = temp
 
-        s.reverse()
+        # # sol 2
+        # i = 0
+        # j = len(s)-1
+        # while i<j:
+        #     s[i], s[j] = s[j], s[i]
+        #     i=i+1
+        #     j=j-1
+
+
+
+        
 
         
         
