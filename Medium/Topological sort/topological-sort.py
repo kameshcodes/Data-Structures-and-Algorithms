1136dc056a85f7f29a3dc292278202e28617c6e5
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-06-02 20:28:05 +0530

    Updated Solution - GfG to GitHub

--- Medium/Topological sort/topological-sort.py
+++ Medium/Topological sort/topological-sort.py
@@ -1,31 +1,76 @@
 from queue import LifoQueue
+from queue import Queue
 class Solution:
     
-    #Function to return list containing vertices in Topological order.
     
-    def dfs(self, V, adj, visited, stack, node):
-        visited[node] = 1
-        for node_ in adj[node]:
-            if visited[node_]==0:
-                self.dfs(V, adj, visited, stack, node_)
-        stack.put(node)
-
-
-
-
-    def topoSort(self, V, adj):
-        stack = LifoQueue(maxsize=V)
-        visited = [0 for i in range(V)]
-        for i in range(V):
-            if visited[i] == 0:
-                self.dfs(V, adj, visited, stack, node=i)
+    
+    #Function to return list containing vertices in Topological order.
+    def get_indegree(self, V, adj):
         
+        indegree = [0 for i in range(V)]
+        
+        for i in range(V):
+            for node in adj[i]:
+                indegree[node]+=1
+        return indegree
+        
+        
+    def topoSort(self, V, adj):
+        
+        indegree = self.get_indegree(V, adj)
+        visited = [0 for i in range(V)]
+        q = Queue(maxsize = V)
+        
+        for i in range(V):
+            if indegree[i] == 0:
+                q.put(i)
+                visited[i]=1
         
         topo = []
-        while stack.qsize()>0:
-            topo.append(stack.get())
         
+        while q.empty()==False:
+            
+            popped_node = q.get()
+            topo.append(popped_node)
+            
+            for node in adj[popped_node]:
+                
+                indegree[node]-=1
+                if visited[node]==0 and indegree[node] == 0:
+                    q.put(node)
+                    visited[node]+=1
+                    
         return topo
+        
+        
+        
+        
+            
+    
+    #Function to return list containing vertices in Topological order.
+    # def dfs(self, V, adj, visited, stack, node):
+    #     visited[node] = 1
+    #     for node_ in adj[node]:
+    #         if visited[node_]==0:
+    #             self.dfs(V, adj, visited, stack, node_)
+    #     stack.put(node)
+
+
+
+
+    # def topoSort(self, V, adj):
+    #     stack = LifoQueue(maxsize=V)
+    #     visited = [0 for i in range(V)]
+    #     for i in range(V):
+    #         if visited[i] == 0:
+    #             self.dfs(V, adj, visited, stack, node=i)
+        
+        
+    #     topo = []
+    #     while stack.qsize()>0:
+    #         topo.append(stack.get())
+        
+    #     return topo
         
     
 
