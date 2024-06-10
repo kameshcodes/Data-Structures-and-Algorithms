63a6031e73703bd7ca7b0b1852c2389a93d29514
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-06-10 23:32:14 +0530

    Time: 1567 ms (7.87%) | Memory: 32.3 MB (67.93%) - LeetSync

--- leetcode-problems/239-sliding-window-maximum/sliding-window-maximum.py
+++ leetcode-problems/239-sliding-window-maximum/sliding-window-maximum.py
@@ -7,15 +7,17 @@ class Solution:
         N = len(nums)
 
         while j < N:
-            while q and nums[q[-1]] <= nums[j]:
+            # while q and nums[q[-1]] <= nums[j]:
+            while q and q[-1] < nums[j]:
                 q.pop()
-            q.append(j)
-            if j - i + 1 > k:
-                q.pop(0) 
+            # q.append(j)
+            q.append(nums[j])
             if j - i + 1 == k:
-                ans.append(nums[q[0]]) 
-                if i == q[0]:
+                ans.append(q[0]) 
+                # ans.append(nums[q[0]]) 
+                if nums[i] == q[0]:
                     q.pop(0)
                 i += 1
             j += 1
+
         return ans
