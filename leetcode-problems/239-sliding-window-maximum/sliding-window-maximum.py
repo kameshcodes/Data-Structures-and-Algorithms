695563f6656b11e4567a81d7eb9a48d064c0c562
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-06-10 23:28:44 +0530

    Time: 1199 ms (21.00%) | Memory: 32.2 MB (85.63%) - LeetSync

--- leetcode-problems/239-sliding-window-maximum/sliding-window-maximum.py
+++ leetcode-problems/239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,29 +1,21 @@
-from collections import deque
-
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        q = deque()  # Use deque from collections for O(1) pops from the front
+        q = []
         ans = []
         i = 0
         j = 0
         N = len(nums)
 
         while j < N:
-            # Maintain elements in decreasing order in the deque
-            while q and q[-1] < nums[j]:
+            while q and nums[q[-1]] <= nums[j]:
                 q.pop()
-            q.append(nums[j])
-
-            if j - i + 1 < k:
-                j += 1
-            elif j - i + 1 == k:
-                ans.append(q[0])  # The front of the deque is the maximum
-
-                # If the element at the front of the deque is the one leaving the window, remove it
-                if nums[i] == q[0]:
-                    q.popleft()  # Use popleft() for deque
-
+            q.append(j)
+            if j - i + 1 > k:
+                q.pop(0) 
+            if j - i + 1 == k:
+                ans.append(nums[q[0]]) 
+                if i == q[0]:
+                    q.pop(0)
                 i += 1
-                j += 1
-
+            j += 1
         return ans
