2324643e44e3ac7272c82b80d0a988b7bd5434ca
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-07-31 12:30:33 +0530

    Time: 390 ms (20.27%) | Memory: 0B (100.00%) - LeetSync

--- leetcode-problems/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ leetcode-problems/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-SELECT employee_id
-FROM Employees
-WHERE manager_id NOT IN (SELECT DISTINCT(employee_id) FROM Employees) and salary <30000
-ORDER BY employee_id
+SELECT e1.employee_id
+FROM Employees e1
+LEFT JOIN Employees e2
+ON e1.manager_id = e2.employee_id
+WHERE e1.salary < 30000 and e2.employee_id is null and e1.manager_id is not null
+ORDER BY e1.employee_id
