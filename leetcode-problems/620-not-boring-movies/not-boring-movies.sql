a0c80fc3d771788ea754fa7d8b6c59702bc9214d
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-08-06 23:47:29 +0530

    Time: 209 ms (83.24%) | Memory: 0B (100.00%) - LeetSync

--- leetcode-problems/620-not-boring-movies/not-boring-movies.sql
+++ leetcode-problems/620-not-boring-movies/not-boring-movies.sql
@@ -3,6 +3,6 @@
 # movies with 1. odd number_id that is 2. not boring
 
 SELECT *
-FROM Cinema
-WHERE id%2!=0 AND description!='boring'
-ORDER BY rating DESC;
+FROM (SELECT * FROM Cinema WHERE id%2!=0) as d
+WHERE d.description!='boring'
+ORDER BY d.rating DESC;
