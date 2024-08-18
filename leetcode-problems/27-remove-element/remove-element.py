7d51d99fe214c42137171badd37f8a92fa49dbfa
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-08-18 19:11:09 +0530

    Time: 41 ms (27.99%) | Memory: 16.4 MB (66.90%) - LeetSync

--- leetcode-problems/27-remove-element/remove-element.py
+++ leetcode-problems/27-remove-element/remove-element.py
@@ -1,14 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
-        length = len(nums)
-        while i<length:
+        while i<len(nums):
             if nums[i]==val:
-                nums.remove(val)
-                length = len(nums)
+                nums.pop(i)
             else:
                 i=i+1
-
-
-        return len(nums)
-        
