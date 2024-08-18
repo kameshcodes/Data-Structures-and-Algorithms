716ca69e570f1878789ea24c378b698374516180
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-08-18 19:04:21 +0530

    Time: 42 ms (41.09%) | Memory: 16.6 MB (10.96%) - LeetSync

--- leetcode-problems/88-merge-sorted-array/merge-sorted-array.py
+++ leetcode-problems/88-merge-sorted-array/merge-sorted-array.py
@@ -3,33 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # idx1 = 0
-        # idx2 = 0
-        # while idx1<len(nums1) and idx2<n:
-        #     if nums1[idx1] >= nums2[idx2]:
-        #         nums1.insert(idx1, nums2[idx2])
-        #         nums1.pop()
-        #         idx2+=1
-        #         m+=1
-        #     elif idx1>=m:
-        #         nums1.insert(idx1, nums2[idx2])
-        #         nums1.pop()
-        #         idx2+=1
-        #     idx1+=1
+        idx1 = 0
+        idx2 = 0
+        while idx1<len(nums1) and idx2<n:
+            if nums1[idx1] >= nums2[idx2]:
+                nums1.insert(idx1, nums2[idx2])
+                nums1.pop()
+                idx2+=1
+                m+=1
+            elif idx1>=m:
+                nums1.insert(idx1, nums2[idx2])
+                nums1.pop()
+                idx2+=1
+            idx1+=1
 
-        k = m+n-1
-        i = m-1
-        j = n-1
-        while i>=0 and j>=0:
-            if nums2[j]>=nums1[i]:
-                nums1[k]=nums2[j]
-                k-=1
-                j-=1
-            else:
-                nums1[k] = nums1[i]
-                k-=1
-                i-=1
-        while j>=0:
-            nums1[k] = nums2[j]
-            k=k-1
-            j=j-1
+        # k = m+n-1
+        # i = m-1
+        # j = n-1
+        # while i>=0 and j>=0:
+        #     if nums2[j]>=nums1[i]:
+        #         nums1[k]=nums2[j]
+        #         k-=1
+        #         j-=1
+        #     else:
+        #         nums1[k] = nums1[i]
+        #         k-=1
+        #         i-=1
+        # while j>=0:
+        #     nums1[k] = nums2[j]
+        #     k=k-1
+        #     j=j-1
