57f56a0d3f45f29b6455e7eef1dff1e4c10529dc
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-08-18 20:35:33 +0530

    Time: 179 ms (21.53%) | Memory: 18.1 MB (19.30%) - LeetSync

--- leetcode-problems/169-majority-element/majority-element.py
+++ leetcode-problems/169-majority-element/majority-element.py
@@ -1,12 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = {}
+        freq = {}
         for num in nums:
-            if num not in count:
-                count[num] = 1
+            if num in freq:
+                freq[num]+=1
             else:
-                count[num] = count[num] + 1
-
-        for num in list(count.keys()):
-            if count[num]>len(nums)//2:
+                freq[num]=1
+        
+        for num in nums:
+            if freq[num]>(len(nums)//2):
                 return num
