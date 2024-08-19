95319926774291970b7582f955961ab3f61a912d
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-08-19 13:13:02 +0530

    Time: 158 ms (56.12%) | Memory: 28.1 MB (24.97%) - LeetSync

--- leetcode-problems/189-rotate-array/rotate-array.py
+++ leetcode-problems/189-rotate-array/rotate-array.py
@@ -31,21 +31,26 @@ class Solution:
         # while l < r:
         #     nums[l], nums[r] = nums[r], nums[l]
         #     l += 1
-        #     r -= 1
+        # #     r -= 1
         
 
-        k = k % len(nums) 
-        def reverse_till_idx(l, r):
-            while l < r:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
+        # k = k % len(nums) 
+        # def reverse_till_idx(l, r):
+        #     while l < r:
+        #         nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        #         r -= 1
         
-        l, r = 0, len(nums)-1
-        reverse_till_idx(l, r)
+        # l, r = 0, len(nums)-1
+        # reverse_till_idx(l, r)
 
-        l, r = 0, k-1
-        reverse_till_idx(l, r)
+        # l, r = 0, k-1
+        # reverse_till_idx(l, r)
 
-        l, r = k, len(nums)-1
-        reverse_till_idx(l, r)
+        # l, r = k, len(nums)-1
+        # reverse_till_idx(l, r)
+
+        k = k % len(nums)
+        nums.reverse()
+        nums[0:k] = reversed(nums[0:k])
+        nums[k:len(nums)] = reversed(nums[k:len(nums)])
