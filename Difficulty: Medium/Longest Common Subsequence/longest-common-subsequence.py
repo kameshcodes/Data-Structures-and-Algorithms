b1d9c6d46c4f89111d0962c3f647ad8ba5f87f6c
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2024-08-27 05:14:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.py
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.py
@@ -5,26 +5,44 @@ class Solution:
     #Function to find the length of longest common subsequence in two strings.
     def lcs(self, n, m, str1, str2):
         # code here
-        t = [[-1 for i in range(m+1)] for j in range(n+1)]
+        # t = [[-1 for i in range(m+1)] for j in range(n+1)]
         
-        def lcs_helper(n, m):
-            if n==0 or m==0:
-                return 0
+        # def lcs_helper(n, m):
+        #     if n==0 or m==0:
+        #         return 0
                 
-            if t[n][m]!=-1:
-                return t[n][m]
+        #     if t[n][m]!=-1:
+        #         return t[n][m]
             
-            if str1[n-1] == str2[m-1]:
-                t[n][m] = 1 + lcs_helper(n-1, m-1)
+        #     if str1[n-1] == str2[m-1]:
+        #         t[n][m] = 1 + lcs_helper(n-1, m-1)
                 
-            else:
-                t[n][m] = max(lcs_helper(n-1, m), lcs_helper(n, m-1))
+        #     else:
+        #         t[n][m] = max(lcs_helper(n-1, m), lcs_helper(n, m-1))
         
-            return t[n][m]
+        #     return t[n][m]
             
             
-        return lcs_helper(n, m)
+        # return lcs_helper(n, m)
+        
+        n = len(str1)
+        m = len(str2)
 
+        t = [[-1 for j in range(n+1)] for i in range(m+1)]
+
+        for i in range(m+1):
+            for j in range(n+1):
+                if i == 0 or j == 0:
+                    t[i][j] = 0
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if str1[j-1] == str2[i-1]:
+                    t[i][j] = 1 + t[i-1][j-1]
+                else:
+                    t[i][j] = max(t[i-1][j], t[i][j-1])
+        return t[m][n]       
+        
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
