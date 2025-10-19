4963de96ad95d88add746dea841a89b499c2a555
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2025-10-20 03:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.68%) - LeetSync

--- leetcode-problems/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ leetcode-problems/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,12 +1,27 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        dp = [0] * (n+1)
-        dp[0:2] = [0, 1, 1]
+        # dp = [0] * (n+1)
+        # dp[0:2] = [0, 1, 1]
 
-        if n<3:
-            return dp[n]
-        for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        # if n<3:
+        #     return dp[n]
+        # for i in range(3, n+1):
+        #     dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
         
-        return dp[n]
+        # return dp[n]
+
+        memo = {}
+        def count(n, memo):
+            if n in memo:
+                return memo[n]
+            if n==0:
+                return 0
+            elif n==1 or n==2:
+                return 1
+            else:
+                memo[n] = count(n-1, memo) + count(n-2, memo) + count(n-3, memo)
+                return memo[n]
+        
+        return count(n, memo)
+
 
