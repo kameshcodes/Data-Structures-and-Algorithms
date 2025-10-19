ca9da835e2d489d48e0f3f565ab71fa6602c23fe
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2025-10-19 23:46:15 +0530

    Time: 41 ms (52.76%) | Memory: 17.8 MB (41.19%) - LeetSync

---
+++ leetcode-problems/1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<=1:
+            return n
+
+        dp = [-1] * (n+1)
+        for i in range(n+1):
+            if i==0 or i==1:
+                dp[i] = i
+            else:
+                dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+            
