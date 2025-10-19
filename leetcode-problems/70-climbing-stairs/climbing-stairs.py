0e45d0b3c323d69397c71737f5adde65a51ce712
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2025-10-19 22:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.37%) - LeetSync

--- leetcode-problems/70-climbing-stairs/climbing-stairs.py
+++ leetcode-problems/70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,11 @@
 class Solution:
-    dp = {}  # class variable, shared by ALL instances
-
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n == 1:
+        if n==0 or n==1:
             return 1
-        if n in Solution.dp:
-            return Solution.dp[n]
-        Solution.dp[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
-        return Solution.dp[n]
+
+        dp = [-1]*(n+1)
+        dp[0], dp[1] = 1, 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
