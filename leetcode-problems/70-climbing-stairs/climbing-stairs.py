06e0626823d257f7c7608a592d678b329f110167
Kamesh Dubey <kamesh.iitbombay@gmail.com>
2025-10-19 22:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.37%) - LeetSync

--- leetcode-problems/70-climbing-stairs/climbing-stairs.py
+++ leetcode-problems/70-climbing-stairs/climbing-stairs.py
@@ -3,9 +3,10 @@ class Solution:
         if n==0 or n==1:
             return 1
 
-        dp = [-1]*(n+1)
-        dp[0], dp[1] = 1, 1
+        a = 1
+        b = 1
         for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+            # dp[i] = dp[i-1] + dp[i-2]
+            a, b = a+b, a
 
-        return dp[n]
+        return a
